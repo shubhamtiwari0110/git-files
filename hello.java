fc51998cb16cceb23822120256f9c8610f41e4c0
piyush-tiwari-glitch <vikram.pande9591@hotmail.com>
2021-08-05 11:42:39 +0530

    hello modified

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class Employee{
 public static void main(Stirng[] args)
 System.out.println("hello");
-System.out.println("hello again in git repo");
+}
+public static int add(int a,inty b){
+return a+b;
 }
